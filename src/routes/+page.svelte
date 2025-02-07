b584ed459a41f9bcd632eb661bf0df6c31f5eff4
Schasch <schasch.dev@protonmail.com>
2025-02-07 00:53:06 +0000

    construction page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<style>
+  @import './index.css';
+</style>
+
+<div class="background" id="background"></div>
+    <div class="content">
+        <h1>Under Construction</h1>
+        <p>This page is currently being built. Please check back later.</p>
+        <p>In the meantime, you can find a replacement <a href="https://startpage.schasch.xyz">here</a>.</p>
+    </div>
+
+    <script>
+        import { onMount } from 'svelte';
+
+        onMount(() => {
+            const background = document.getElementById('background');
+
+            document.addEventListener('mousemove', (e) => {
+                const x = (e.clientX / window.innerWidth) * 100;
+                const y = (e.clientY / window.innerHeight) * 100;
+                background.style.transform = `translate(-${x / 15}%, -${y / 15}%)`;
+            });
+        });
+    </script>
